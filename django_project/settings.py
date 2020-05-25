4635b9f4d594fbcd49b446450a3e800049236ea1
siddharth <sidagl158@gmail.com>
2020-05-25 17:48:54 +0530

    Updated STATIC_ROOT

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL= '/media/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
