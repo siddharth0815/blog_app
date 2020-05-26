40cf754e030bb5ede97aa384fcd89bbf1b32b107
siddharth <sidagl158@gmail.com>
2020-05-26 14:31:09 +0530

    demo django project

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,7 +130,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sidagl158@gmail.com'
-EMAIL_HOST_PASSWORD = 'ofnjsczpinurzych'
-
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
 LOGIN_URL = 'login'
